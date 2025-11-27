550b180d5b584ff107a0577b0965863d8490a3a7
Smiley Amc <jonesjacobson23@gmail.com>
2025-11-27 08:35:08 +0300

    feat: ajout page Call-to-action

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CallToAction from "@/components/call-to-action";
 import ContentSection from "@/components/content-1";
 import Features from "@/components/features-1";
 import HeroSection from "@/components/hero-section";
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <Features />
       <ContentSection />
+      <CallToAction />
     </>
   )
 }
